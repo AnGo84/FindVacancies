6e2e84c7c8e19308a325a9aa65656939d0ba9a12
AnGo <ango1984@gmail.com>
2020-11-13 13:49:18 +0200

    fix validation annotations

--- src/main/java/ua/findvacancies/mvc/model/viewdata/ViewSearchParams.java
+++ src/main/java/ua/findvacancies/mvc/model/viewdata/ViewSearchParams.java
@@ -1,9 +1,9 @@
 package ua.findvacancies.mvc.model.viewdata;
 
-import org.hibernate.validator.constraints.NotBlank;
-import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import java.util.HashSet;
 import java.util.Set;
 
