ed0f14c2e9a20de3b16b379d7247658abd05d5a9
AnGo84 <ango1984@gmail.com>
2022-06-23 15:26:24 +0300

    add insert

---
+++ src/main/resources/db/migration/V1_1_1__Inseart.sql
@@ -0,0 +1,15 @@
+INSERT INTO `cloudXDB`.`vacancies`
+(`title`,
+ `salary`,
+ `city`,
+ `company_name`,
+ `site_name`,
+ `url`,
+ `id`)
+VALUES ('Junior'
+            '500',
+        'Odessa',
+        'comp2',
+        'site2',
+        'site2.com',
+        3);
